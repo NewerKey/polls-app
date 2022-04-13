832e3f74eec9ce70225ce6d8502567ecd250014b
Yvette Nartey <newerkey@gmail.com>
2022-04-13 16:28:48 +0200

    Setup Database connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,17 @@
-const Express = require("express");
+const Express = require('express');
 
-const setupMiddleware = require("./setup/middleware");
+const setupMiddleware = require('./setup/middleware');
+
+const setupDatabase = require('./setup/database');
 
 //Create express instance
 const app = Express();
 
 setupMiddleware(app);
-
-// Start server
-app.listen(4000, () => {
-  console.log("Server started on port 4000");
-});
+setupDatabase()
+  .then((client) => {
+    app.listen(4000, () => {
+      console.log('Server started on port 4000');
+    });
+  })
+  .catch(console.error);
--- backend/src/setup/database.js
+++ backend/src/setup/database.js
@@ -1,5 +1,10 @@
 const Mongodb = require('mongodb');
 
+//Connection string
+const uri = 'mongodb://localhost/poll-app';
+
 module.exports = () => {
-  const client = new Mongodb.MongoClient();
+  const client = new Mongodb.MongoClient(uri, { useUnifiedTopology: true });
+
+  return client.connect(); //establish connection and return a promise
 };
